833f75e23088f895d15ce6c0f846ddbeb9a4209c
mark <mark@rgames.jp>
2020-07-15 15:50:20 +0900

    add comment

--- src/cmd/game/upload.js
+++ src/cmd/game/upload.js
@@ -7,12 +7,14 @@ const view = require('../../util/view');
 const action = async (args, options) => {
   const gameId = args.gameId;
   const bundle = args.bundle;
+  const comment = options.comment;
 
   const onProgress = view.gameUpload.onProgress();
 
   const result = await api.uploadGame({
     gameId,
     bundle,
+    comment,
     onProgress,
   });
 
@@ -24,6 +26,7 @@ program
   .description('Upload game bundle')
   .argument('<game_id>', 'Game id')
   .argument('<bundle>', 'Bundle')
+  .option('--comment <comment>', 'Comment')
   .option('--token <token>', 'Access token')
   .option('--output <output>', 'Output target')
   .action(wrapper(action));
