ed7191a74022c1cc17c03de9c4ddcb348a80316f
mark <mark@rgames.jp>
2020-12-21 15:58:23 +0900

    remove version

--- src/view/game-describe.js
+++ src/view/game-describe.js
@@ -16,40 +16,10 @@ const render = ({output, game}) => {
 
       table.push('Title', _.get(game, 'metadata.en.title'));
       table.push('Game ID', _.get(game, 'id'));
-      table.push('Version', renderVersion(game));
       table.push('Last Updated', renderLastUpdated(game));
 
       table.draw();
   }
-};
-
-const renderVersion = (game) => {
-  let str = '';
-  const versions = _.get(game, 'rollout.versions');
-
-  let total = 0;
-
-  for (let i = 0; i < versions.length; i++) {
-    const {
-      weight,
-    } = versions[i];
-
-    total += weight;
-  }
-
-  for (let i = 0; i < versions.length; i++) {
-    const {
-      version,
-    } = versions[i];
-
-    str += `${version}`;
-
-    if (i !== versions.length - 1) {
-      str += ', ';
-    }
-  }
-
-  return str;
 };
 
 const renderLastUpdated = (game) => {
